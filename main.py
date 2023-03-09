a5f9a7417576649469d339ba1f993deb6865d3e2
Amir <asaman125125@gmail.com>
2023-03-09 19:41:08 +0000

    - Upper and Lower Arm joint angles are now calculated according to RULA - Implemented a method that calculates the angles based on three points - Added the detection of elbow and wrist landmarks - Slight tweaks for code cleanup and performance -Started the implementation of wrist position estimation

--- main.py
+++ main.py
@@ -14,18 +14,39 @@ def findDistance(x1, y1, x2, y2):
 
 
 # Calculate angle
-def findAngle(x1, y1, x2, y2):
+def findAngle(x1, y1, x2, y2): # Finds angle from 2 points
     theta = m.acos((y2 - y1) * (-y1) / (m.sqrt(
         (x2 - x1) ** 2 + (y2 - y1) ** 2) * y1))
     degree = int(180 / m.pi) * theta
     return degree
 
 
+def findAngle2(x1, y1, x2, y2, x3, y3): # Finds angle from 3 points
+    # Calculate two vectors
+    v1 = [x2 - x1, y2 - y1]
+    v2 = [x3 - x2, y3 - y2]
+
+    # Calculate dot product
+    dot_product = v1[0] * v2[0] + v1[1] * v2[1]
+
+    # Calculate magnitudes
+    v1_mag = m.sqrt(v1[0] ** 2 + v1[1] ** 2)
+    v2_mag = m.sqrt(v2[0] ** 2 + v2[1] ** 2)
+
+    # Calculate cosine of angle using dot product and magnitudes
+    cos_angle = dot_product / (v1_mag * v2_mag)
+
+    # Calculate angle in degrees
+    angle_deg = m.degrees(m.acos(cos_angle))
+
+    return angle_deg
+
+
 # Initialise Notify
 notify = Notify()
 run_once = 0
 
-sleep_time = 1
+sleep_time = 0
 
 total_neck_and_torso_score = 0
 total_torso_score = 0
@@ -35,8 +56,12 @@ current_neck_score_data = [] # = [time, neck angle, RULA Score]
 current_torso_score_data = []
 # cumulative_neck_torso_score_data = []
 current_neck_torso_score_data = [] #time, neck score, torso score, Overall according to RULA
+
 cumulative_neck_score_data = [] # = [time, cumulative RULA score]
 cumulative_torso_score_data = []
+
+current_upper_arm_score_data = []
+current_lower_arm_score_data = []
 
 # Initilise frame counters
 bad_frames = 0
@@ -56,6 +81,10 @@ pink = (255, 0, 255)
 # Initialise mediapipe pose class
 mp_pose = mp.solutions.pose
 pose = mp_pose.Pose()
+
+# Initialise mediapipe hands class
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
 
 cap = cv2.VideoCapture(0)
 
@@ -77,13 +106,16 @@ while cap.isOpened():
 
     # Process the image
     keypoints = pose.process(image)
+    keypoints2 = hands.process(image)
 
     # Convert the image back to BGR
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
     # lm (LandMark) and lmPose to represent the mediapipe pose landmarks
     lm = keypoints.pose_landmarks
+    lm2 = keypoints2.multi_hand_landmarks
     lmPose = mp_pose.PoseLandmark
+    lmHands = mp_hands.HandLandmark
 
     # Get the landmark coordinates
     # Left shoulder
@@ -98,6 +130,15 @@ while cap.isOpened():
     # Left hip
     l_hip_x = int(lm.landmark[lmPose.LEFT_HIP].x * w)
     l_hip_y = int(lm.landmark[lmPose.LEFT_HIP].y * h)
+    # Left Elbow
+    l_elbow_x = int(lm.landmark[lmPose.LEFT_ELBOW].x * w)
+    l_elbow_y = int(lm.landmark[lmPose.LEFT_ELBOW].y * h)
+    # Left Wrist
+    l_wrist_x = int(lm.landmark[lmPose.LEFT_WRIST].x * w)
+    l_wrist_y = int(lm.landmark[lmPose.LEFT_WRIST].y * h)
+    # Hand Middle Finger knuckle todo
+    # middle_finger_knuckle_x = int(lm2.landmark[lmHands.MIDDLE_FINGER_MCP].x * w)
+    # middle_finger_knuckle_y = int(lm2.landmark[lmHands.MIDDLE_FINGER_MCP].y * h)
 
     # Calculate distance between left shoulder and right shoulder points
     offset = findDistance(l_shldr_x, l_shldr_y, r_shldr_x, r_shldr_y)
@@ -112,6 +153,9 @@ while cap.isOpened():
     neck_angle = findAngle(l_shldr_x, l_shldr_y, l_ear_x, l_ear_y)
     torso_angle = findAngle(l_hip_x, l_hip_y, l_shldr_x, l_shldr_y)
 
+    upper_arm_angle = findAngle(l_elbow_x, l_elbow_y, l_shldr_x, l_shldr_y + 40) # Change +40 later when calibrating
+    lower_arm_angle = findAngle2(l_wrist_x, l_wrist_y, l_elbow_x, l_elbow_y, l_shldr_x, l_shldr_y + 40) # Change +40 later when calibrating
+
     # Draw landmarks
     cv2.circle(image, (l_shldr_x, l_shldr_y), 7, pink, -1)
     cv2.circle(image, (l_ear_x, l_ear_y), 7, yellow, -1)
@@ -122,10 +166,15 @@ while cap.isOpened():
 
     cv2.circle(image, (l_hip_x, l_hip_y - 100), 7, yellow, -1)
 
+    cv2.circle(image, (l_elbow_x, l_elbow_y), 7, dark_blue, -1)
+    cv2.circle(image, (l_wrist_x, l_wrist_y), 7, dark_blue, -1)
+
+    # cv2.circle(image, (middle_finger_knuckle_x, middle_finger_knuckle_y), 7, pink, -1) todo
+
     # Text string for display
     angle_text_string = 'Neck : ' + str(int(neck_angle)) + '  Torso : ' + str(int(torso_angle))
 
-
+    cv2.putText(image, angle_text_string, (10, 30), font, 0.9, pink, 2)
 
     # Assess posture and score it based on RULA
     if torso_angle == 0:
@@ -135,8 +184,6 @@ while cap.isOpened():
 
         cumulative_torso_score_data.append((round((time.time() - start), 2), total_torso_score))
 
-        cv2.putText(image, angle_text_string, (10, 30), font, 0.9, blue, 2)
-        cv2.putText(image, str(int(neck_angle)), (l_shldr_x + 10, l_shldr_y), font, 0.9, blue, 2)
         cv2.putText(image, str(int(torso_angle)), (l_hip_x + 10, l_hip_y), font, 0.9, blue, 2)
 
         # Join landmarks.
@@ -153,8 +200,6 @@ while cap.isOpened():
 
         cumulative_torso_score_data.append((round((time.time() - start), 2), total_torso_score))
 
-        cv2.putText(image, angle_text_string, (10, 30), font, 0.9, green, 2)
-        cv2.putText(image, str(int(neck_angle)), (l_shldr_x + 10, l_shldr_y), font, 0.9, green, 2)
         cv2.putText(image, str(int(torso_angle)), (l_hip_x + 10, l_hip_y), font, 0.9, green, 2)
 
         # Join landmarks.
@@ -171,8 +216,6 @@ while cap.isOpened():
 
         cumulative_torso_score_data.append((round((time.time() - start), 2), total_torso_score))
 
-        cv2.putText(image, angle_text_string, (10, 30), font, 0.9, yellow, 2)
-        cv2.putText(image, str(int(neck_angle)), (l_shldr_x + 10, l_shldr_y), font, 0.9, yellow, 2)
         cv2.putText(image, str(int(torso_angle)), (l_hip_x + 10, l_hip_y), font, 0.9, yellow, 2)
 
         # Join landmarks.
@@ -189,20 +232,14 @@ while cap.isOpened():
 
         cumulative_torso_score_data.append((round((time.time() - start), 2), total_torso_score))
 
-        cv2.putText(image, angle_text_string, (10, 30), font, 0.9, red, 2)
-        cv2.putText(image, str(int(neck_angle)), (l_shldr_x + 10, l_shldr_y), font, 0.9, red, 2)
         cv2.putText(image, str(int(torso_angle)), (l_hip_x + 10, l_hip_y), font, 0.9, red, 2)
 
         # Join landmarks.
         cv2.line(image, (l_hip_x, l_hip_y), (l_shldr_x, l_shldr_y), red, 4)
         cv2.line(image, (l_hip_x, l_hip_y), (l_hip_x, l_hip_y - 100), red, 4)
 
-        #time.sleep(sleep_time)
+        time.sleep(sleep_time)
 
-
-
-
-    #decide if the neck posture is good or bad
 
     if neck_angle < 10 and neck_angle >= 0:
         current_neck_score_data.append((round((time.time() - start), 2), torso_angle, 1))
@@ -211,9 +248,7 @@ while cap.isOpened():
 
         cumulative_neck_score_data.append((round((time.time() - start), 2), total_neck_score))
 
-        cv2.putText(image, angle_text_string, (10, 30), font, 0.9, green, 2)
         cv2.putText(image, str(int(neck_angle)), (l_shldr_x + 10, l_shldr_y), font, 0.9, green, 2)
-        cv2.putText(image, str(int(torso_angle)), (l_hip_x + 10, l_hip_y), font, 0.9, green, 2)
 
         # Join landmarks.
         cv2.line(image, (l_shldr_x, l_shldr_y), (l_ear_x, l_ear_y), green, 4)
@@ -227,9 +262,7 @@ while cap.isOpened():
 
         cumulative_neck_score_data.append((round((time.time() - start), 2), total_neck_score))
 
-        cv2.putText(image, angle_text_string, (10, 30), font, 0.9, yellow, 2)
         cv2.putText(image, str(int(neck_angle)), (l_shldr_x + 10, l_shldr_y), font, 0.9, yellow, 2)
-        cv2.putText(image, str(int(torso_angle)), (l_hip_x + 10, l_hip_y), font, 0.9, yellow, 2)
 
         # Join landmarks.
         cv2.line(image, (l_shldr_x, l_shldr_y), (l_ear_x, l_ear_y), yellow, 4)
@@ -243,13 +276,70 @@ while cap.isOpened():
 
         cumulative_neck_score_data.append((round((time.time() - start), 2), total_neck_score))
 
-        cv2.putText(image, angle_text_string, (10, 30), font, 0.9, red, 2)
         cv2.putText(image, str(int(neck_angle)), (l_shldr_x + 10, l_shldr_y), font, 0.9, red, 2)
-        cv2.putText(image, str(int(torso_angle)), (l_hip_x + 10, l_hip_y), font, 0.9, red, 2)
 
         # Join landmarks.
         cv2.line(image, (l_shldr_x, l_shldr_y), (l_ear_x, l_ear_y), red, 4)
         cv2.line(image, (l_shldr_x, l_shldr_y), (l_shldr_x, l_shldr_y - 100), red, 4)
+
+
+    if upper_arm_angle <= 20:
+        current_upper_arm_score_data.append((round((time.time() - start), 2), upper_arm_angle, 1))
+
+        cv2.putText(image, str(int(upper_arm_angle)), (l_shldr_x - 30, l_shldr_y), font, 0.9, blue, 2)
+
+        # Join landmarks.
+        cv2.line(image, (l_elbow_x, l_elbow_y), (l_shldr_x, l_shldr_y), blue, 4)
+
+    if upper_arm_angle > 20 and upper_arm_angle <= 45:
+        current_upper_arm_score_data.append((round((time.time() - start), 2), upper_arm_angle, 2))
+
+        cv2.putText(image, str(int(upper_arm_angle)), (l_shldr_x - 30, l_shldr_y), font, 0.9, green, 2)
+
+        # Join landmarks.
+        cv2.line(image, (l_elbow_x, l_elbow_y), (l_shldr_x, l_shldr_y), green, 4)
+
+    if upper_arm_angle > 45 and upper_arm_angle <= 90:
+        current_upper_arm_score_data.append((round((time.time() - start), 2), upper_arm_angle, 3))
+
+        cv2.putText(image, str(int(upper_arm_angle)), (l_shldr_x - 30, l_shldr_y), font, 0.9, yellow, 2)
+
+        # Join landmarks.
+        cv2.line(image, (l_elbow_x, l_elbow_y), (l_shldr_x, l_shldr_y), yellow, 4)
+
+    if upper_arm_angle > 90:
+        current_upper_arm_score_data.append((round((time.time() - start), 2), upper_arm_angle, 4))
+
+        cv2.putText(image, str(int(upper_arm_angle)), (l_shldr_x - 30, l_shldr_y), font, 0.9, red, 2)
+
+        # Join landmarks.
+        cv2.line(image, (l_elbow_x, l_elbow_y), (l_shldr_x, l_shldr_y), red, 4)
+
+
+    if lower_arm_angle >= 60 and lower_arm_angle < 100:
+        current_lower_arm_score_data.append((round((time.time() - start), 2), lower_arm_angle, 1))
+
+        cv2.putText(image, str(int(upper_arm_angle)), (l_elbow_x - 30, l_elbow_y), font, 0.9, green, 2)
+
+        # Join landmarks.
+        cv2.line(image, (l_elbow_x, l_elbow_y), (l_wrist_x, l_wrist_y), green, 4)
+
+    if lower_arm_angle >= 0 and lower_arm_angle < 60:
+        current_lower_arm_score_data.append((round((time.time() - start), 2), lower_arm_angle, 2))
+
+        cv2.putText(image, str(int(lower_arm_angle)), (l_elbow_x - 30, l_elbow_y), font, 0.9, yellow, 2)
+
+        # Join landmarks.
+        cv2.line(image, (l_elbow_x, l_elbow_y), (l_wrist_x, l_wrist_y), yellow, 4)
+
+    if lower_arm_angle > 100:
+        current_lower_arm_score_data.append((round((time.time() - start), 2), lower_arm_angle, 2))
+
+        cv2.putText(image, str(int(lower_arm_angle)), (l_elbow_x - 30, l_elbow_y), font, 0.9, red, 2)
+
+        # Join landmarks.
+        cv2.line(image, (l_elbow_x, l_elbow_y), (l_wrist_x, l_wrist_y), red, 4)
+
 
     # Calculate the duration of posture
     bad_time = (1 / fps) * bad_frames
